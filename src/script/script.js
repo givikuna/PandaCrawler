e607fc11f97df8c48fa5e78735f24e1159927e12
Givi Tsvariani <thommaster10@gmail.com>
2023-04-26 13:10:56 -0400

    Update script.js

---
+++ src/script/script.js
@@ -0,0 +1,27 @@
+function loader() {
+  var xhttp = new XMLHttpRequest(); //use to connect to the servers
+	var url = "@dynamicLink:8090/?request=userinfo";
+	xhttp.open("GET", url, true);
+
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				const userInfo = JSON.parse(this.responseText);
+        const translationData = userInfo[0];
+        const extensions = userInfo[1];
+        const pageAdditions = userInfo[2];
+        const miscellaneous = userInfo[3];
+        translator(translationData);
+			} else {
+				serverDisconnectErr();
+			}
+		}
+	};
+	xhttp.send();
+}
+
+function translator(translationData) {
+  for (let i = 0; i < translationData.length; i++) {
+    Document.getElementById(translationData[i].id).value = translationData[i].value;
+  }
+}
